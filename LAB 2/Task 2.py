5faada5d9e9874638ba93fab29b64d1232d904ff
Khalid Al Rifai <ing.khaledhr@gmail.com>
2020-12-19 19:40:24 +0800

    Create Task 2.py

---
+++ LAB 2/Task 2.py
@@ -0,0 +1,8 @@
+no_list = [22,68,90,78,90,88]
+
+def average(x):
+#complete the function's body to return the average
+    length=len(no_list)
+    return sum(no_list)/length
+
+print(average(no_list))
