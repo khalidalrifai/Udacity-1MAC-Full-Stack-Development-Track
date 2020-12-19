9c8e8bbc759eac638e82022c0a7abfd096b0f722
Khalid Al Rifai <ing.khaledhr@gmail.com>
2020-12-19 19:39:23 +0800

    Update and rename Task 1.txt to Task 1.py

--- LAB 2/Task 1.py
+++ LAB 2/Task 1.py
@@ -1,6 +1,8 @@
 text = "this is not a reversed text"
+
 def reverse(x):
 #complete this function so that it takes string x as an input
 #and returns its reverse
-return x[::-1]
+    return x[::-1]
+
 print("the reversed text is: "+reverse(text))
